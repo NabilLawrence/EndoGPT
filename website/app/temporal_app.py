fe1fd224a0279d660b192479c3dcfab1dc004d64
Oscar Rincon <oandres.rincon@udea.edu.co>
2023-03-15 12:53:01 +0000

    test for image

--- website/app/temporal_app.py
+++ website/app/temporal_app.py
@@ -89,7 +89,8 @@ html_string = '<iframe src="https://giphy.com/embed/QBd2kLB5qDmysEXre9" width="4
 from PIL import Image
 
 st.write(os.getcwd())
-image = Image.open('../images/Logo.png')
+image = Image.open('Logo.png')
+#image = Image.open('../images/Logo.png')
 
 st.image(image, width=300)
 
